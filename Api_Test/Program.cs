b57715f2c66d4078d7657b4d9184c07712214a16
John Dominguez <john-dominguez@stonybrook.edu>
2018-05-16 16:48:23 -0400

    init commmit

---
+++ Api_Test/Program.cs
@@ -0,0 +1,53 @@
+﻿using FortniteApi;
+using FortniteApi.Data;
+using System;
+using System.Media;
+using Library;
+
+namespace Api_Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GetInfo();
+            Console.ReadKey();
+        }
+
+        public static async void GetInfo()
+        {
+            using (FortniteClient client = new FortniteClient("bb93b0a2-0a92-4969-89aa-5180e589a065"))
+            {
+                var response = await client.FindPlayerAsync(Platform.Pc, "Sougyo");
+
+                
+//                Console.WriteLine(response.Stats["P2"]["score"].ValueInt);
+//                Console.WriteLine(response.RecentMatches.Count);
+
+                Console.WriteLine($"Solo Wins: {response.Stats[Playlist.Solo][Stat.Top1].ValueInt}");
+                response.Stats[Playlist.Solo][Stat.]
+//
+//                var keys = response.Stats.Keys;
+//                foreach (var key in keys)
+//                {
+//                    foreach (var key2 in response.Stats[key].Keys)
+//                    {
+//                        Console.WriteLine($"{key2} : {response.Stats[key][key2]}");
+//                    }
+//
+//                    Console.WriteLine("---");
+//                }
+//
+//                foreach (var match in response.RecentMatches.AsReadOnly())
+//                {
+//                    Console.WriteLine(match.DateCollected);
+//                    Console.WriteLine(match.Kills);
+//                }
+
+//                Serializer.Serialize(response, 0);
+
+
+            }
+        }
+    }
+}
---
+++ Api_Test/Serializer.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.IO;
+namespace Library
+{
+    public class Serializer
+    {
+        public static void Serialize(Object type, int val, string filename= "C:\\Users\\John\\Coding\\_Revature\\testing\\Api_Test\\data2.json")
+        {
+            JsonSerializer serializer = new JsonSerializer
+            {
+                NullValueHandling = NullValueHandling.Ignore,
+                ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore
+            };
+
+            
+            using (StreamWriter streamWriter = new StreamWriter(filename))
+            using (JsonWriter writer = new JsonTextWriter(streamWriter))
+            {
+                Console.WriteLine(writer.ToString());
+                serializer.Serialize(writer, type);
+            }
+        }
+
+        public static string Serialize<T>(T type)
+        {
+            return JsonConvert.SerializeObject(type);
+        }
+
+        public static List<T> Deserialize<T>(string filename)
+        {
+            var jsonFile = File.ReadAllText(filename);
+            return JsonConvert.DeserializeObject<List<T>>(jsonFile);
+        }
+       
+    }
+}
